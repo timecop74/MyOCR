2ad50c5633de407e5e0a69d5b8ecc78e69d59f35
timecop74 <relaxgames@mail.ru>
2021-05-21 13:14:40 +0300

    Альфа

--- Data/OCRService.cs
+++ Data/OCRService.cs
@@ -1,23 +1,30 @@
+using Aspose.OCR;
 using Microsoft.AspNetCore.Components.Forms;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Threading.Tasks;
-using Aspose.OCR;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace MyOCR.Data
 {
     public class OCRService
     {
+        private AsposeOcr ac = new AsposeOcr();
+        private MemoryStream ms = new MemoryStream();
 
         public async Task<List<Rectangle>> GetRectAsync(IBrowserFile file)
         {
-            AsposeOcr ac = new AsposeOcr();
-            MemoryStream ms = new MemoryStream();
+            ms.Close();
+            ms = new MemoryStream();
             var t = file.OpenReadStream(file.Size);
             await t.CopyToAsync(ms);
             List<Rectangle> rects = ac.GetRectangles(ms);
             return rects;
+        }
+
+        public async Task<List<string>> Recognize(List<Rectangle> areas)
+        {
+            return ac.RecognizeImage(ms, areas);
         }
     }
 }
